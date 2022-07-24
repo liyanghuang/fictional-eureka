d51123856af2cc7b64164e44cf3d1cf7e657a16c
Liyang Huang <liyangclementhuang@gmail.com>
2022-07-23 19:42:08 -0700

    adding actual plugin file

---
+++ src/main/java/com/fe/plugin/PluginMain.java
@@ -0,0 +1,12 @@
+package com.fe.plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginMain extends JavaPlugin{   
+
+    @Override
+    public void onEnable() {
+    }
+    @Override
+    public void onDisable() {
+    }
+}
